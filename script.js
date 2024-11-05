54da00d023314163bdfc709a2548b5c2600eeea7
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-11-05 18:37:22 +0200

    add error message

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const formContainer = document.getElementById('form-container');
 const modal = document.getElementById('modal');
-const closeModal = document.getElementById('closeModal');
-const modalData = document.getElementById('modalData');
+const closeModal = document.getElementById('close-modal');
+const data = document.getElementById('data');
 
 
 formContainer.addEventListener('submit', (event) => {
@@ -17,9 +17,10 @@ formContainer.addEventListener('submit', (event) => {
 
   fields.forEach((input) => {
     input.style.borderColor = '';
+    input.nextElementSibling.textContent = '';
   });
 
-  const fullNameRegex = /^[А-ЯІЇЄҐа-яіїєґ]{2,20} [А-ЯІЇЄҐа-яіїєґ]\.[А-ЯІЇЄҐа-яіїєґ]\.$/;
+  const fullNameRegex = /^[А-ЯІЇЄҐ][а-яіїєґ]{1,20} [А-ЯІЇЄҐ]\.[А-ЯІЇЄҐ]\.$/;
   const birthDateRegex = /^\d{2}\.\d{2}\.\d{4}$/;
   const addressRegex = /^м\. [А-ЯІЇЄҐа-яіїєґ]{2,20}$/;
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/;
@@ -29,26 +30,31 @@ formContainer.addEventListener('submit', (event) => {
 
   if (!fullNameRegex.test(fields[0].value.trim())) {
     fields[0].style.borderColor = 'red';
+    fields[0].nextElementSibling.textContent = "ПІБ повинен бути у форматі: Прізвище І.Б.";
     incorrectCounter++;
   }
 
   if (!birthDateRegex.test(fields[1].value.trim())) {
     fields[1].style.borderColor = 'red';
+    fields[1].nextElementSibling.textContent = "Дата народження повинна бути у форматі: ДД.ММ.РРРР";
     incorrectCounter++;
   }
 
   if (!addressRegex.test(fields[2].value.trim())) {
     fields[2].style.borderColor = 'red';
+    fields[2].nextElementSibling.textContent = "Адреса повинна починатися з 'м.' і містити назву міста";
     incorrectCounter++;
   }
 
   if (!emailRegex.test(fields[3].value.trim())) {
     fields[3].style.borderColor = 'red';
+    fields[3].nextElementSibling.textContent = "Неправильний формат e-mail";
     incorrectCounter++;
   }
 
   if (!telegramRegex.test(fields[4].value.trim())) {
     fields[4].style.borderColor = 'red';
+    fields[4].nextElementSibling.textContent = "Telegram повинен починатися з '@'";
     incorrectCounter++;
   }
 
@@ -69,7 +75,7 @@ closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
   });
   
-  window.addEventListener('click', (event) => {
+window.addEventListener('click', (event) => {
     if (event.target === modal) {
       modal.style.display = 'none';
     }
